a873af3e461722b04b106a3e55c4d90a9d386bf9
Aarne, Ylönen <ag3483@student.jamk.fi>
2026-04-29 13:30:22 +0300

    erittäin raaka jokseenkin toimiva  local tuotos frontend sivusta

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BoardComponent } from './components/board/board.component'; // 1. Add this import
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BoardComponent], // 2. Add BoardComponent to the imports array
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
---
+++ frontend/src/app/components/ghost/ghost.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ghost',
+  templateUrl: './ghost.component.html',
+  styleUrls: ['./ghost.component.css']
+})
+export class GhostComponent {
+  @Input() position!: { x: number; y: number };
+}
