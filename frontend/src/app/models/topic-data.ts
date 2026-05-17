eedcf67dcea0a46c6d8429c1d133c92fda959722
Aino Seppi <ag2490@student.jamk.fi>
2026-05-17 22:05:59 +0300

    valepalvelin otettu pois käytöstä, arki-kategoriasivujen sisällön haku tietokannasta valepalvelimen sijaan

---
+++ frontend/src/app/models/topic-data.ts
@@ -0,0 +1,6 @@
+export interface TopicData {
+  _id: string;
+  category: string;
+  title: string;
+  content: string[];
+}
--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,22 +1,24 @@
 import { HttpClient } from '@angular/common/http'; // http-olio hakee datan
 import { inject, Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs'; // data haetaan asynkronisesti observablena
-import { Checklist } from '../models/checklist';
-import { Guide } from '../models/guide';
+// import { Checklist } from '../models/checklist';
+// import { Guide } from '../models/guide';
 import { NewBudgetEntry } from '../models/new-budget-entry';
 import { RecurringEntry } from '../models/recurring-entry';
 import { NewRecurringEntry } from '../models/new-recurring-entry';
+import { TopicData } from '../models/topic-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  // Valepalvelimen eli in-memory-web-apin osoitteet. Täältä tulee sisältö arki-osion kategorioihin.
-  private apiurlMoving = 'api/muutto';
-  private apiurlCleaning = 'api/siivous';
-  private apiurlFinances = 'api/talous';
+  // Valepalvelimen eli in-memory-web-apin osoitteet
+  // private apiurlMoving = 'api/muutto';
+  // private apiurlCleaning = 'api/siivous';
+  // private apiurlFinances = 'api/talous';
 
   // Oikean palvelimen osoitteet
+  private apiurlTopics = 'http://localhost:3000/api/topics';
   private apiUrlBudget = 'api/budgets';
   private apiUrlBudgetR = 'api/recurring';
 
@@ -24,17 +26,34 @@ export class DataService {
 
   categoryTitle = signal('');
 
-  getTopicData(category: string): Observable<Checklist[] | Guide[]> {
+  // valepalvelimelta hakuun
+  // getTopicData(category: string): Observable<Checklist[] | Guide[]> {
+  //   switch (category) {
+  //     case 'moving':
+  //       this.categoryTitle.set('Muutto');
+  //       return this.http.get<Checklist[]>(this.apiurlMoving);
+  //     case 'cleaning':
+  //       this.categoryTitle.set('Siivous');
+  //       return this.http.get<Guide[]>(this.apiurlCleaning);
+  //     case 'finances':
+  //       this.categoryTitle.set('Taloudenhallinta');
+  //       return this.http.get<Guide[]>(this.apiurlFinances);
+  //     default:
+  //       throw new Error('Datan hakeminen epäonnistui: tuntematon kategoria');
+  //   }
+  // }
+
+  getTopicData(category: string): Observable<TopicData[]> {
     switch (category) {
       case 'moving':
         this.categoryTitle.set('Muutto');
-        return this.http.get<Checklist[]>(this.apiurlMoving);
+        return this.http.get<TopicData[]>(`${this.apiurlTopics}/${category}`);
       case 'cleaning':
         this.categoryTitle.set('Siivous');
-        return this.http.get<Guide[]>(this.apiurlCleaning);
+        return this.http.get<TopicData[]>(`${this.apiurlTopics}/${category}`);
       case 'finances':
         this.categoryTitle.set('Taloudenhallinta');
-        return this.http.get<Guide[]>(this.apiurlFinances);
+        return this.http.get<TopicData[]>(`${this.apiurlTopics}/${category}`);
       default:
         throw new Error('Datan hakeminen epäonnistui: tuntematon kategoria');
     }
